f917403f1943fe488cab04da1f4527a216c7a7f1
milinsky <mail@milinsky.ru>
2018-12-29 17:15:40 +1000

    Добавление конфигов

--- app/Config/kernel.members.php
+++ app/Config/kernel.members.php
@@ -1,4 +1,6 @@
 <?php
 
+Config::register('Kernel', 'Kernel', 'Kernel');
 Config::register('Kernel', 'Request', 'Kernel')->subscribe('Kernel.Kernel.Ready', 'run');
 Config::register('Kernel', 'Router', 'Kernel')->subscribe('Kernel.Request.UriReady', 'run');
+Config::register('Kernel', 'Controller', 'Kernel')->subscribe('Kernel.Router.RouteReady', 'runController', ['Kernel.Auth' => 'AuthReady']);
