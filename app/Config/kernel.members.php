ef51b6eda07e1fbb16c251cfbc3234bccef8c96f
milinsky <mail@milinsky.ru>
2019-01-12 17:56:31 +1000

    Правки

--- app/Config/kernel.members.php
+++ app/Config/kernel.members.php
@@ -2,5 +2,5 @@
 
 Config::register('Kernel', 'Kernel', 'Kernel');
 Config::register('Kernel', 'Request', 'Kernel')->subscribe('Kernel.Kernel.Ready', 'run');
-Config::register('Kernel', 'Router', 'Kernel')->subscribe('Kernel.Request.UriReady', 'run');
+Config::register('Kernel', 'Router', 'Kernel')->subscribe('Kernel.Request.RequestReady', 'run');
 Config::register('Kernel', 'Controller', 'Kernel')->subscribe('Kernel.Router.RouteReady', 'runController', ['Kernel.Auth' => 'AuthReady']);
