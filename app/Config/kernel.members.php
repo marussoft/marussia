a468f25ddd864b62d7ec01cee054bd7e2950ff0c
milinsky <mail@milinsky.ru>
2019-01-22 15:54:27 +1000

    Исправления ошибок

--- app/Config/kernel.members.php
+++ app/Config/kernel.members.php
@@ -3,6 +3,13 @@
 use Marussia\HttpKernel\Config as Config;
 
 Config::register('Kernel', 'Kernel', 'Kernel');
+
 Config::register('Kernel', 'Request', 'Kernel')->subscribe('Kernel.Kernel.Ready', 'run');
+
 Config::register('Kernel', 'Router', 'Kernel')->subscribe('Kernel.Request.RequestReady', 'run');
-Config::register('Kernel', 'Controller', 'Kernel')->subscribe('Kernel.Router.RouteReady', 'runController', ['Kernel.Auth' => 'AuthReady']);
+
+Config::register('Kernel', 'Auth', 'Kernel')->subscribe('Kernel.Kernel.Ready', 'init');
+
+Config::register('Kernel', 'Controller', 'Kernel')
+    ->subscribe('Kernel.Request.RequestReady', 'setRequest')
+    ->subscribe('Kernel.Router.RouterReady', 'runController', ['Kernel.Auth' => 'Ready']);
---
+++ app/Controllers/PageController/Home.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers\PageController;
+
+use Marussia\HttpKernel\App as App;
+use Marussia\HttpKernel\Base\Controller as Controller;
+
+class Home extends Controller
+{
+
+    public function run($route)
+    {
+        echo 'Hello, World';
+    }
+
+}
