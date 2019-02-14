e0e08d8427e0e513ffaa7e6ac56ca9e7dd510d37
milinsky <mail@milinsky.ru>
2019-02-14 11:22:40 +1000

    Новые роуты

--- app/Routes/default.php
+++ app/Routes/default.php
@@ -4,40 +4,40 @@ use Marussia\Router\Route as Route;
 
 
 // Маршруты глфвной страницы
-Route::add('Page', 'Home', '(^/)');
+Route::get('Page', 'Home', '(^/)');
 
 
 // Маршруты авторизации
-Route::add('Auth', 'Login', '(^login$)');
+Route::post('Auth', 'Login', '(^login$)');
 
-Route::add('Auth', 'Logout', '(^logout$)');
+Route::get('Auth', 'Logout', '(^logout$)');
 
 
 // Маршруты для списка и просмотра записей
-Route::add('Page', 'View', '(^[a-z0-9\-]+\.html$)', [], false, true);
+Route::get('Page', 'View', '(^[a-z0-9\-]+\.html$)', [], false, true);
 
-Route::add('?', 'Index', '(^[a-z]+$)', ['page' => 1]);
+Route::get('?', 'Index', '(^[a-z]+$)', ['page' => 1]);
 
-Route::add('?', 'Index', '([a-z]+)/page-([0-9]+)', ['page' => '([0-9]+$)']);
+Route::get('?', 'Index', '([a-z]+)/page-([0-9]+)', ['page' => '([0-9]+$)']);
 
-Route::add('?', 'View', '([a-z]+)/([0-9]+)-([a-z0-9\-]+).html', ['id' => '([0-9]+)']);
+Route::get('?', 'View', '([a-z]+)/([0-9]+)-([a-z0-9\-]+).html', ['id' => '([0-9]+)']);
 
 
 // Маршруты для добавления, редактирования и удаления
-Route::add('?', '?', '(^[a-z]+)/([a-z]+)/([0-9]+)/([0-9]+)($)', ['id-1' => '([0-9]+)', 'id-2' => '([0-9]+)']);
+Route::get('?', '?', '(^[a-z]+)/([a-z]+)/([0-9]+)/([0-9]+)($)', ['id-1' => '([0-9]+)', 'id-2' => '([0-9]+)']);
 
-Route::add('?', 'AddCat', '(^[a-z]+)/addcat/([0-9]+)', ['id' => '([0-9]+)']);
+Route::get('?', 'AddCat', '(^[a-z]+)/addcat/([0-9]+)', ['id' => '([0-9]+)']);
 
-Route::add('?', 'AddCat', '(^[a-z]+)/addcat($)');
+Route::get('?', 'AddCat', '(^[a-z]+)/addcat($)');
 
-Route::add('?', 'DeleteCat', '(^[a-z]+)/deletecat/([0-9]+)', ['id' => '([0-9]+)']);
+Route::get('?', 'DeleteCat', '(^[a-z]+)/deletecat/([0-9]+)', ['id' => '([0-9]+)']);
 
-Route::add('?', '?', '(^[a-z]+)/([a-z]+)/([0-9]+)', ['id' => '([0-9]+)']);
+Route::get('?', '?', '(^[a-z]+)/([a-z]+)/([0-9]+)', ['id' => '([0-9]+)']);
 
-Route::add('?', 'Add', '(^[a-z]+/add$)');
+Route::get('?', 'Add', '(^[a-z]+/add$)');
 
 
 // Маршруты для вложенных страниц (категорий)
-Route::add('?', 'Index', '(^[a-z0-9]+)/(([a-z0-9\-]+/)+|([a-z0-9\-_]+)+$)', ['page' => 1], true);
+Route::get('?', 'Index', '(^[a-z0-9]+)/(([a-z0-9\-]+/)+|([a-z0-9\-_]+)+$)', ['page' => 1], true);
 
-Route::add('?', 'View', '(^[a-z0-9]+)/(([a-z0-9\-]+/)+)([0-9]+)-([a-z0-9\-]+).html', ['id' => '([0-9]+)'], true);
+Route::get('?', 'View', '(^[a-z0-9]+)/(([a-z0-9\-]+/)+)([0-9]+)-([a-z0-9\-]+).html', ['id' => '([0-9]+)'], true);
